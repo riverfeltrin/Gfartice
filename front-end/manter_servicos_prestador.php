c3710ee38323fbd0dc176750805a31139c5927d6
riverfeltrin <erivelton.feltrin@outlook.com>
2018-04-06 12:03:24 -0300

    Revert "Revert "Atualização de views""
    This reverts commit 8b053e81f3bf9424924ef545fdb5b1c37d0f1ee5.

--- front-end/manter_servicos_prestador.php
+++ front-end/manter_servicos_prestador.php
@@ -1,27 +1,43 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
+</head>
+<body>
+	<body style="background-color: black;">
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Gfartice</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$bdName = "bd_gfartice";
+  <div class="collapse navbar-collapse" id="navbarColor02">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="manter_servicos.php">Serviços</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="manter_usuario.php">Cadastrar-se</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="sobre.php">Sobre</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
+	<div style="margin-right: 20%; margin-left: 20%; margin-top: 5%; background-color: #DAA520; border-radius: 20px;">
+	<form style="margin-left: 5%;" method="POST" action="inserir_servico.php">
+		
 
-	//criar conexão
-	$conn = new mysqli($servername, $username, $password, $bdName);
+	</form>
 
-	if ($conn -> connect_error) {
-		die("Erro: ". $conn -> connect_error);
-	}else{
-		echo "Conectado";
-	}
-
-	$sql = "DROP DATABASE bd_gfartice";
-	if ($conn -> query($sql) == TRUE) {
-		echo "Banco de dados EXCLUIDO";
-	}else{
-		echo "Erro na EXCLUSÃO ". $conn ->error;
-	}
-
-	$conn -> close();
-	
-  ?>
+	</div>
+</body>
+</html>
